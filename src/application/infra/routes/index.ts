1f6d7be0b1a224ccd072255d51632fdefd60c509
Paulo Mariano <paulomariano2501@gmail.com>
2024-01-27 01:00:07 -0300

    👌 IMPROVE: add and configure swagger for docs

--- src/application/infra/routes/index.ts
+++ src/application/infra/routes/index.ts
@@ -5,12 +5,15 @@ import { eventRoutes } from './event';
 import { authRoutes } from './auth';
 import { AuthUseCase } from '../../use-cases/auth/AuthUseCase';
 import AuthMiddleware from '../../infra/middlewares/auth';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './../../../swagger.json';
 
 const appRoutes = Router();
 
 const authService = new AuthUseCase();
 const authMiddleware = new AuthMiddleware(authService);
 
+appRoutes.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 appRoutes.use('/auth', authRoutes);
 
 appRoutes.use(authMiddleware.authenticateToken);
